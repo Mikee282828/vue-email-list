73b1aea5cca86cc75e52015f30c31c85e2dbb4e2
michele laptop <micheledong2803@gmail.com>
2024-05-15 18:54:48 +0200

    consegna

--- main.js
+++ main.js
@@ -3,7 +3,23 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            mail: []
         }
+    },
+    methods: {
+        richiesta() {
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(
+                    (result) => {
+                        this.mail.push(result.data.response);
+                    }
+                );
+            }
+
+        }
+    },
+    mounted() {
+        console.log("App montata");
+        this.richiesta();
     }
 }).mount('#app')
